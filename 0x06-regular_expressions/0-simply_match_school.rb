604669a55b6d5db6ceb10e7f106f05283db09827
esraasay <esraa.krobeen@gmail.com>
2024-01-31 03:11:00 +0300

    done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+# Check if an argument is provided
+if ARGV.empty?
+	puts "Usage: ruby script.rb <string>"
+	exit 1
+  end
+  
+  # Extract the argument from the command line
+  input_string = ARGV[0]
+  
+  # Define the regular expression pattern
+  pattern = /School/
+  
+  # Use the scan method to find matches in the input string
+  matches = input_string.scan(pattern)
+  
+  # Output the matches found
+  puts matches.join
+  
